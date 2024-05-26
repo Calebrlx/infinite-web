8fdb20a2ca470b737f240f907ad095a9f114d6d0
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-05-25 18:35:44 -0800

    stuff

--- src/providers/openai/getOpenAI.ts
+++ src/providers/openai/getOpenAI.ts
@@ -1,26 +1,47 @@
 import { OpenAI } from 'openai'
 
-// don't do this at home!
-// if we deploy one day to the cloud, we MUST rewrite this..
-export const persisted = {
-  apiKey: '',
-  model: '',
-}
-
 export const getOpenAI = async (apiKey?: string): Promise<OpenAI> => {
-  // don't do this at home!
-  // if we deploy one day to the cloud, we MUST rewrite this..
-  persisted.apiKey = apiKey || persisted.apiKey
+  const apiKeyFromEnv = process.env.OPENAI_API_KEY;
+  const finalApiKey = apiKey || apiKeyFromEnv;
 
-  if (!persisted.apiKey) { throw new Error(`missing API key, we can't call OpenAI`)}
+  if (!finalApiKey) {
+    throw new Error('Missing API key, we can\'t call OpenAI');
+  }
 
   const openai = new OpenAI({
-    apiKey: persisted.apiKey,
+    apiKey: finalApiKey,
     baseURL: "https://api.openai.com/v1",
-
-    // it's ~fine, chill out OpenAI we are running a Desktop app
     dangerouslyAllowBrowser: true,
-  })
+  });
 
-  return openai
+  return openai;
 }
+
+
+
+// import { OpenAI } from 'openai'
+
+// // don't do this at home!
+// // if we deploy one day to the cloud, we MUST rewrite this..
+// export const persisted = {
+//   apiKey: '',
+//   model: '',
+// }
+
+// export const getOpenAI = async (apiKey?: string): Promise<OpenAI> => {
+//   // don't do this at home!
+//   // if we deploy one day to the cloud, we MUST rewrite this..
+//   persisted.apiKey = apiKey || persisted.apiKey
+
+//   if (!persisted.apiKey) { throw new Error(`missing API key, we can't call OpenAI`)}
+
+//   const openai = new OpenAI({
+//     apiKey: persisted.apiKey,
+//     baseURL: "https://api.openai.com/v1",
+
+//     // it's ~fine, chill out OpenAI we are running a Desktop app
+//     dangerouslyAllowBrowser: true,
+//   })
+
+//   return openai
+// }
